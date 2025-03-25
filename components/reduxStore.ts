24c7885cc815c1b8011561f67ffdb8b10ea0a5d8
DockerVM-FMI <cipherporu9@gmail.com>
2025-03-25 09:05:49 +0000

    feat: allow adding custom items to sales

--- components/reduxStore.ts
+++ components/reduxStore.ts
@@ -3,15 +3,17 @@ import productReducer from "./Product/productSlice";
 import productCategoryReducer from "./Product/categorySlice";
 import storeSliceReducer from "./Store/storeSlice";
 import paymentMethodReducer from "./PaymentMethod/paymentMethodSlice";
+import orderReducer from "./Order/orderSlice";
 import { listenerMiddleware } from "./reduxListenerMiddleware";
 import { apiSlice } from "./apiSlice";
 
 export const reduxStore = configureStore({
   reducer: {
+    order: orderReducer,
     product: productReducer,
-    productCategory: productCategoryReducer,
     store: storeSliceReducer,
     paymentMethod: paymentMethodReducer,
+    productCategory: productCategoryReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
