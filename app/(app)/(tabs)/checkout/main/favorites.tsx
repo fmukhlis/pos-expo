a4842c8d4976ad3834aed06dac577e679b5159c6
DockerVM-FMI <cipherporu9@gmail.com>
2025-04-23 09:54:27 +0000

    chore: remove ItemContext in favorites.tsx

--- app/(app)/(tabs)/checkout/main/favorites.tsx
+++ app/(app)/(tabs)/checkout/main/favorites.tsx
@@ -1,29 +1,16 @@
-import { View, Text, ScrollView } from "react-native";
-import React, { useContext } from "react";
-import { ItemContext } from "@/contexts/ItemContext";
+import React from "react";
+
+import { View, ScrollView } from "react-native";
+
 import { Icon } from "@/components/Icon";
 
 const Favorites = () => {
-  const { items } = useContext(ItemContext);
-
   return (
     <ScrollView className="flex-1 bg-white pt-[70]">
       <View className="flex-row flex-wrap gap-2 p-2">
-        {items.map((item, index) => (
-          <View
-            key={item.id}
-            className={`basis-[40%] flex-grow h-24 bg-gray-100 border border-gray-300 rounded justify-center items-center`}
-          >
-            <Text className="text-lg font-bold text-gray-500/80">
-              {item.name}
-            </Text>
-          </View>
-        ))}
-        {items.length < 20 && (
-          <View className="basis-[40%] flex-grow h-24 bg-gray-100 border border-gray-300 rounded justify-center items-center">
-            <Icon name="add" className="text-gray-300" size={40} />
-          </View>
-        )}
+        <View className="basis-[40%] flex-grow h-24 bg-gray-100 border border-gray-300 rounded justify-center items-center">
+          <Icon name="add" className="text-gray-300" size={40} />
+        </View>
       </View>
     </ScrollView>
   );
