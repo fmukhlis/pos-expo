a16aef71fc33f68d0253513d1598f9520713e50e
DockerVM-FMI <cipherporu9@gmail.com>
2025-05-12 04:07:56 +0000

    chore: use rootReducer in redux store configuration

--- components/reduxStore.ts
+++ components/reduxStore.ts
@@ -1,21 +1,11 @@
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
-import productReducer from "./Product/productSlice";
-import productCategoryReducer from "./Product/categorySlice";
-import storeSliceReducer from "./Store/storeSlice";
-import paymentMethodReducer from "./PaymentMethod/paymentMethodSlice";
-import orderReducer from "./Order/orderSlice";
-import { listenerMiddleware } from "./reduxListenerMiddleware";
+
 import { apiSlice } from "./apiSlice";
+import { listenerMiddleware } from "./reduxListenerMiddleware";
+import rootReducer from "./rootReducer";
 
 export const reduxStore = configureStore({
-  reducer: {
-    order: orderReducer,
-    product: productReducer,
-    store: storeSliceReducer,
-    paymentMethod: paymentMethodReducer,
-    productCategory: productCategoryReducer,
-    [apiSlice.reducerPath]: apiSlice.reducer,
-  },
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .prepend(listenerMiddleware.middleware)
