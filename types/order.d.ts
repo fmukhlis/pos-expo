c6b7a298e6df43048c95d8de0bbb8a94b6a34c6f
DockerVM-FMI <cipherporu9@gmail.com>
2025-05-12 03:54:32 +0000

    feat: add Order type definition

---
+++ types/order.d.ts
@@ -0,0 +1,56 @@
+import { PaymentMethod } from "./payment-method";
+import { Product, ProductModifier, ProductVariant } from "./product";
+import { User } from "./user";
+
+export interface Item {
+  id: number;
+  note: string | null;
+  customAmount: number | null;
+  isCanceled: 0 | 1;
+  cancelReason: string | null;
+  canceledAt: string | null;
+  quantity: number;
+  discount: number | null;
+
+  product: Product | null;
+  variantId: number | null;
+  selectedModifierIds: number[] | null;
+}
+
+export interface Order {
+  id: number;
+  cashAmount: number;
+  orderType: "Dine In" | "Take Away";
+  status: "Paid" | "Billed";
+  tableNumber: number | null;
+  customer: null;
+  paymentMethod: PaymentMethod;
+  orderedProducts: Item[];
+  createdAt: string;
+  user: User;
+}
+
+export type ItemPayload = CustomItemPayload | StandardItemPayload;
+
+export interface CustomItemPayload {
+  note?: string;
+  customAmount: number;
+  discount?: number;
+  quantity: number;
+}
+
+export interface StandardItemPayload
+  extends Omit<CustomItemPayload, "customAmount"> {
+  variantId: string;
+  modifierIds: string[];
+}
+
+export interface OrderPayload {
+  cashAmount: number;
+  orderType: "Dine In" | "Take Away";
+  status: "Paid" | "Billed";
+  tableNumber?: number;
+  customerId?: number;
+  paymentMethodId: number;
+  orderedProducts: ItemPayload[];
+}
