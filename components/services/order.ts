71d82ca887d81ca87e8e860da3ef5267cbaeb282
DockerVM-FMI <cipherporu9@gmail.com>
2025-06-03 07:06:14 +0000

    feat: add infinite query endpoint for fetching transaction history and fix mismatched tags

--- components/services/order.ts
+++ components/services/order.ts
@@ -1,13 +1,35 @@
 import { apiSlice } from "../apiSlice";
-import { Order, OrderPayload } from "@/types/order";
+import { TransactionHistory, Order, OrderPayload } from "@/types/order";
 
-const orderAPI = apiSlice.injectEndpoints({
+export const orderAPI = apiSlice.injectEndpoints({
   overrideExisting: true,
   endpoints: (build) => ({
-    getOrders: build.query<Order[], GetOrdersArg>({
-      query: ({ storeId }) => `/stores/${storeId}/orders`,
-      transformResponse: (data: { data: Order[] }) => data.data,
-      providesTags: [{ type: "Permission", id: "LIST" }],
+    getTransactionHistory: build.infiniteQuery<
+      {
+        data: TransactionHistory[];
+        meta: { next_cursor: string; prev_cursor: string };
+      },
+      GetTransactionHistoryArg,
+      string
+    >({
+      infiniteQueryOptions: {
+        initialPageParam: "",
+        getNextPageParam: (lastPage) => {
+          if (!lastPage.meta.next_cursor) {
+            return undefined;
+          }
+          return lastPage.meta.next_cursor;
+        },
+        getPreviousPageParam: (firstPage) => {
+          if (!firstPage.meta.prev_cursor) {
+            return undefined;
+          }
+          return firstPage.meta.prev_cursor;
+        },
+      },
+      query: ({ pageParam: cursor, queryArg: { storeId } }) =>
+        `/stores/${storeId}/transaction-history?cursor=${cursor}`,
+      providesTags: [{ type: "Order", id: "LIST" }],
     }),
     storeOrder: build.mutation<Order, StoreOrderArg>({
       query: ({ storeId, ...body }) => ({
@@ -16,15 +38,18 @@ const orderAPI = apiSlice.injectEndpoints({
         body,
       }),
       transformResponse: (data: { data: Order }) => data.data,
-      invalidatesTags: [{ type: "Permission", id: "LIST" }],
+      invalidatesTags: [{ type: "Order", id: "LIST" }],
     }),
   }),
 });
 
-export const { useGetOrdersQuery, useStoreOrderMutation } = orderAPI;
+export const { useGetTransactionHistoryInfiniteQuery, useStoreOrderMutation } =
+  orderAPI;
 
-interface GetOrdersArg {
+interface GetTransactionHistoryArg {
   storeId: number;
+  refreshKey?: number;
+  date?: string;
 }
 
-type StoreOrderArg = GetOrdersArg & OrderPayload;
+type StoreOrderArg = { storeId: number } & OrderPayload;
