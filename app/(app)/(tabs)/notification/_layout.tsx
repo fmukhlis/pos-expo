152cc949b7742bcadcf6899e84b796ad14ddcf67
DockerVM-FMI <cipherporu9@gmail.com>
2025-06-03 07:12:16 +0000

    fix: ensure store has been selected before navigating to notification screen

--- app/(app)/(tabs)/notification/_layout.tsx
+++ app/(app)/(tabs)/notification/_layout.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 
 import { Stack } from "expo-router";
+import { useAppSelector } from "@/components/reduxHooks";
+import { Redirect } from "expo-router";
 
 const NotificationLayout = () => {
+  const storeId = useAppSelector(({ store }) => store.selectedStoreId);
+
+  if (!storeId) {
+    return <Redirect href={"/account"} />;
+  }
+
   return (
     <Stack
       screenOptions={{
