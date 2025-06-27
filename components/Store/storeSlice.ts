75905892c79c75dab3ddb2f0dd385521a307213c
Fajar Mukhlis Imananda <cipherporu9@gmail.com>
2025-06-27 20:51:47 +0700

    feat(PrintReceipt): add global states and reducers in store slice for printer settings

--- components/Store/storeSlice.ts
+++ components/Store/storeSlice.ts
@@ -1,7 +1,10 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IBLEPrinter } from "react-native-thermal-receipt-printer-image-qr";
 
 const initialState: StoreState = {
   selectedStoreId: null,
+  isAutoPrintReceipt: false,
+  selectedBluetoothPrinter: null,
 };
 
 const storeSlice = createSlice({
@@ -11,14 +14,29 @@ const storeSlice = createSlice({
     setSelectedStoreId: (state, action: PayloadAction<number | null>) => {
       state.selectedStoreId = action.payload;
     },
+    selectBluetoothPrinter: (
+      state,
+      action: PayloadAction<IBLEPrinter | null>
+    ) => {
+      state.selectedBluetoothPrinter = action.payload;
+    },
+    setAutoPrintReceipt: (state, action: PayloadAction<boolean>) => {
+      state.isAutoPrintReceipt = action.payload;
+    },
   },
   extraReducers: (build) => {},
 });
 
-export const { setSelectedStoreId } = storeSlice.actions;
+export const {
+  setSelectedStoreId,
+  setAutoPrintReceipt,
+  selectBluetoothPrinter,
+} = storeSlice.actions;
 
 export default storeSlice.reducer;
 
 interface StoreState {
   selectedStoreId: number | null;
+  isAutoPrintReceipt: boolean;
+  selectedBluetoothPrinter: IBLEPrinter | null;
 }
