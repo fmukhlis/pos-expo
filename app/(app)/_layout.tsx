9a0c6e7be3833e0bca90b9ef0a0d9d267d872169
Fajar Mukhlis Imananda <cipherporu9@gmail.com>
2025-07-03 17:48:27 +0700

    feat(PrinterPreferences): auto-load printer preferences on app startup

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,12 +1,78 @@
+import React from "react";
+import * as SecureStore from "expo-secure-store";
+
+import { BLEPrinter } from "react-native-thermal-receipt-printer-image-qr";
 import { Redirect, Stack } from "expo-router";
 
 import VerifyEmail from "@/components/VerifyEmail";
+import LoadingPage from "@/components/LoadingPage";
 
 import { useSession } from "@/contexts/SessionContext";
-import LoadingPage from "@/components/LoadingPage";
+import { useAppDispatch } from "@/components/reduxHooks";
+import {
+  selectBluetoothPrinter,
+  setAutoPrintReceipt,
+  setBase64ReceiptLogo,
+} from "@/components/Store/storeSlice";
 
 function AuthLayout() {
   const { session, user, sessionLoading } = useSession();
+
+  const dispatch = useAppDispatch();
+
+  React.useEffect(() => {
+    const init = async () => {
+      const base64ReceiptLogo = await SecureStore.getItemAsync(
+        "base64ReceiptLogo"
+      );
+      const isAutoPrintReceipt = await SecureStore.getItemAsync(
+        "isAutoPrintReceipt"
+      );
+      const selectedBluetoothPrinterName = await SecureStore.getItemAsync(
+        "selectedBluetoothPrinterName"
+      );
+      const selectedBluetoothPrinterInnerMacAddress =
+        await SecureStore.getItemAsync(
+          "selectedBluetoothPrinterInnerMacAddress"
+        );
+      if (base64ReceiptLogo !== null) {
+        dispatch(setBase64ReceiptLogo(base64ReceiptLogo));
+      }
+      if (isAutoPrintReceipt !== null) {
+        dispatch(setAutoPrintReceipt(isAutoPrintReceipt === "true"));
+      }
+      if (
+        selectedBluetoothPrinterName !== null &&
+        selectedBluetoothPrinterInnerMacAddress
+      ) {
+        dispatch(
+          selectBluetoothPrinter({
+            device_name: selectedBluetoothPrinterName,
+            inner_mac_address: selectedBluetoothPrinterInnerMacAddress,
+          })
+        );
+        BLEPrinter.init()
+          .then(() => {
+            BLEPrinter.connectPrinter(selectedBluetoothPrinterInnerMacAddress)
+              .then(
+                () => {},
+                (err) => {
+                  console.error(
+                    `Error when trying to connect to the printer\n(${err})`
+                  );
+                }
+              )
+              .catch((err) => {
+                console.error(
+                  `Error when trying to connect to the printer\n(${err})`
+                );
+              });
+          })
+          .catch((err) => {});
+      }
+    };
+    init();
+  }, []);
 
   if (sessionLoading) {
     return <LoadingPage />;
