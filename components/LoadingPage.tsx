f877d5f2e526188c774227a636ac6fe2c7616d49
Fajar Mukhlis Imananda <cipherporu9@gmail.com>
2025-07-03 18:56:43 +0700

    fix(ManageStandardItemModal): set "Original" as fallback option and use selectedItem.name as modal title, with selectedProduct.name as fallback

--- components/LoadingPage.tsx
+++ components/LoadingPage.tsx
@@ -1,26 +1,34 @@
-import React, { ComponentPropsWithoutRef } from 'react'
-import { View, Image, ActivityIndicator } from 'react-native'
+import React, { ComponentPropsWithoutRef } from "react";
+import { View, Image, ActivityIndicator } from "react-native";
 
-import { Colors } from '@/constants/Colors'
-import { useTheme } from '@/contexts/ThemeProvider'
+import { Colors } from "@/constants/Colors";
+import { useTheme } from "@/contexts/ThemeProvider";
 
 const LoadingPage = (props: React.ComponentPropsWithoutRef<typeof View>) => {
-    const { colorScheme } = useTheme()
+  const { colorScheme } = useTheme();
 
-    return (
-        <View {...props} className='flex-1 justify-center items-center'>
-            <Image
-                tintColor={colorScheme === 'dark' ? Colors.dark.highlightedText : Colors.light.highlightedText}
-                source={require('@/assets/images/logo.png')}
-                className='w-[200px] h-[60px] mb-5'
-                resizeMode='contain'
-            />
-            <ActivityIndicator
-                size={'large'}
-                color={colorScheme === 'dark' ? Colors.dark.highlightedText : Colors.light.highlightedText}
-            />
-        </View>
-    )
-}
+  return (
+    <View {...props} className="flex-1 justify-center items-center bg-white">
+      <Image
+        tintColor={
+          colorScheme === "dark"
+            ? Colors.dark.highlightedText
+            : Colors.light.highlightedText
+        }
+        source={require("@/assets/images/logo.png")}
+        className="w-[200px] h-[60px] mb-5"
+        resizeMode="contain"
+      />
+      <ActivityIndicator
+        size={"large"}
+        color={
+          colorScheme === "dark"
+            ? Colors.dark.highlightedText
+            : Colors.light.highlightedText
+        }
+      />
+    </View>
+  );
+};
 
-export default LoadingPage
+export default LoadingPage;
