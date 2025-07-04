a1c366293da0dd7b2dd075e11a5d7846d0112216
Fajar Mukhlis Imananda <cipherporu9@gmail.com>
2025-07-04 19:10:07 +0700

    feat(NetPrinter): add global state selectedNetPrinter to support printing receipts via network printers

--- components/Store/storeSlice.ts
+++ components/Store/storeSlice.ts
@@ -1,10 +1,14 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { IBLEPrinter } from "react-native-thermal-receipt-printer-image-qr";
+import {
+  IBLEPrinter,
+  INetPrinter,
+} from "react-native-thermal-receipt-printer-image-qr";
 
 const initialState: StoreState = {
   selectedStoreId: null,
   base64ReceiptLogo: "",
   isAutoPrintReceipt: false,
+  selectedNetPrinter: null,
   selectedBluetoothPrinter: null,
 };
 
@@ -18,6 +22,9 @@ const storeSlice = createSlice({
     ) => {
       state.selectedBluetoothPrinter = action.payload;
     },
+    selectNetPrinter: (state, action: PayloadAction<INetPrinter | null>) => {
+      state.selectedNetPrinter = action.payload;
+    },
     setAutoPrintReceipt: (state, action: PayloadAction<boolean>) => {
       state.isAutoPrintReceipt = action.payload;
     },
@@ -29,9 +36,10 @@ const storeSlice = createSlice({
 });
 
 export const {
+  selectNetPrinter,
   setAutoPrintReceipt,
-  selectBluetoothPrinter,
   setBase64ReceiptLogo,
+  selectBluetoothPrinter,
 } = storeSlice.actions;
 
 export default storeSlice.reducer;
@@ -40,5 +48,6 @@ interface StoreState {
   selectedStoreId: number | null;
   base64ReceiptLogo: string;
   isAutoPrintReceipt: boolean;
+  selectedNetPrinter: INetPrinter | null;
   selectedBluetoothPrinter: IBLEPrinter | null;
 }
