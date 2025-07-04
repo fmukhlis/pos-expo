4992110b9c98167c103582e1369dfb76f3550dbe
Fajar Mukhlis Imananda <cipherporu9@gmail.com>
2025-07-04 22:24:53 +0700

    feat(PrintReceipt): implement network printer selection and printer disable feature

--- app/(app)/(tabs)/account/printer-settings.tsx
+++ app/(app)/(tabs)/account/printer-settings.tsx
@@ -2,30 +2,41 @@ import React from "react";
 import * as SecureStore from "expo-secure-store";
 
 import { Stack } from "expo-router";
-import { BLEPrinter } from "react-native-thermal-receipt-printer-image-qr";
+import {
+  BLEPrinter,
+  NetPrinter,
+} from "react-native-thermal-receipt-printer-image-qr";
 import {
   View,
   Text,
   Switch,
-  ScrollView,
-  PermissionsAndroid,
   Platform,
+  ScrollView,
+  TouchableOpacity,
+  PermissionsAndroid,
 } from "react-native";
 
+import PrimaryInput from "@/components/PrimaryInput";
 import CustomRadioGroup from "@/components/CustomRadioGroup";
 import LoadingComponent from "@/components/LoadingComponent";
 
 import { Icon } from "@/components/Icon";
+import { MaterialIcons } from "@expo/vector-icons";
+import { PrimaryButtonSM } from "@/components/PrimaryButton";
 import { useAppDispatch, useAppSelector } from "@/components/reduxHooks";
 import {
-  selectBluetoothPrinter,
+  selectNetPrinter,
   setAutoPrintReceipt,
   setBase64ReceiptLogo,
+  selectBluetoothPrinter,
 } from "@/components/Store/storeSlice";
 
 const PrinterSettings = () => {
   const selectedBluetoothPrinter = useAppSelector(
     ({ store }) => store.selectedBluetoothPrinter
+  );
+  const selectedNetPrinter = useAppSelector(
+    ({ store }) => store.selectedNetPrinter
   );
   const isAutoPrintReceipt = useAppSelector(
     ({ store }) => store.isAutoPrintReceipt
@@ -42,6 +53,11 @@ const PrinterSettings = () => {
 
   const [isLoading, setIsLoading] = React.useState(false);
   const [isConnecting, setIsConnecting] = React.useState(false);
+
+  const [netPrinter, setNetPrinter] = React.useState({
+    host: selectedNetPrinter?.host ?? "",
+    port: `${selectedNetPrinter?.port ?? ""}`,
+  });
 
   const [error, setError] = React.useState("");
 
@@ -172,10 +188,7 @@ const PrinterSettings = () => {
 
           <View className="mt-5 mb-2 mx-5">
             <Text className="text-lg  font-bold text-gray-700">
-              Select printer
-            </Text>
-            <Text className="text-base text-gray-700">
-              Currently, it only supports Bluetooth devices.
+              Select bluetooth printer
             </Text>
             <Text className="text-sm mt-2 text-orange-600/70">
               Please make sure to select a real{" "}
@@ -193,40 +206,159 @@ const PrinterSettings = () => {
               <LoadingComponent />
             </View>
           ) : (
-            <CustomRadioGroup
-              className="mx-5 my-2"
-              options={options}
-              renderItem={(label, isSelected) => {
-                return (
-                  <>
-                    <View className="border rounded-full border-gray-300 w-[25] h-[25]">
-                      {isSelected && (
-                        <View className="rounded-full bg-blue-500 w-[18] h-[18] m-auto border border-blue-400" />
-                      )}
-                    </View>
-                    <Text className="ml-2 text-base">{label}</Text>
-                    {isSelected &&
-                      (isConnecting ? (
-                        <LoadingComponent
-                          size={"small"}
-                          className="ml-auto flex-none"
-                        />
-                      ) : (
-                        <Icon
-                          name="checkmark-done"
-                          size={20}
-                          className="ml-auto text-green-500"
-                        />
-                      ))}
-                  </>
-                );
-              }}
-              value={selectedBluetoothPrinter?.inner_mac_address}
-              onValueChange={handleBluetoothPrinterChange}
-            />
+            <>
+              <View className="flex-row mx-5 items-center">
+                <Text className="font-bold text-base text-gray-400">
+                  Disable Bluetooth Printer ?
+                </Text>
+                <TouchableOpacity
+                  disabled={!selectedBluetoothPrinter}
+                  onPress={() => {
+                    BLEPrinter.closeConn()
+                      .then(() => {
+                        dispatch(selectBluetoothPrinter(null));
+                      })
+                      .catch((err) => {
+                        setError(
+                          `Error when closing bluetooth connection\n(${err})`
+                        );
+                      });
+                  }}
+                  className="ml-auto border p-1.5 border-gray-300 bg-gray-100 rounded"
+                >
+                  <MaterialIcons
+                    name="print-disabled"
+                    size={20}
+                    color={"#9ca3af"}
+                  />
+                </TouchableOpacity>
+              </View>
+              <CustomRadioGroup
+                className="mx-5 mt-2"
+                options={options}
+                renderItem={(label, isSelected) => {
+                  return (
+                    <>
+                      <View className="border rounded-full border-gray-300 w-[25] h-[25]">
+                        {isSelected && (
+                          <View className="rounded-full bg-blue-500 w-[18] h-[18] m-auto border border-blue-400" />
+                        )}
+                      </View>
+                      <Text className="ml-2 text-base">{label}</Text>
+                      {isSelected &&
+                        (isConnecting ? (
+                          <LoadingComponent
+                            size={"small"}
+                            className="ml-auto flex-none"
+                          />
+                        ) : (
+                          <Icon
+                            name="checkmark-done"
+                            size={20}
+                            className="ml-auto text-green-500"
+                          />
+                        ))}
+                    </>
+                  );
+                }}
+                value={selectedBluetoothPrinter?.inner_mac_address}
+                onValueChange={handleBluetoothPrinterChange}
+              />
+            </>
           )}
 
-          <View className="flex-row justify-between items-center border-y border-gray-300 py-3 px-5 mt-3">
+          <View className="mx-5 mt-3 mb-3">
+            <Text className="text-lg  font-bold text-gray-700">
+              Select network printer
+            </Text>
+            <Text className="text-sm mt-2 mb-2 text-orange-600/70">
+              You can perform a <Text className="font-bold">Self Test</Text> on
+              the printer to check its IP address. This feature has not been
+              tested yet, so make sure the printer is{" "}
+              <Text className="font-bold">idle</Text> before printing and{" "}
+              <Text className="font-bold">the cable</Text> is properly connected
+              to avoid any issues.
+            </Text>
+            <View className="flex-row items-center">
+              <Text className="font-bold text-base text-gray-400">
+                Disable Network Printer ?
+              </Text>
+              <TouchableOpacity
+                onPress={() => {
+                  NetPrinter.closeConn()
+                    .then(() => {
+                      dispatch(selectNetPrinter(null));
+                    })
+                    .catch((err) => {
+                      setError(
+                        `Error when closing network connection\n(${err})`
+                      );
+                    });
+                }}
+                className="ml-auto border p-1.5 border-gray-300 bg-gray-100 rounded"
+              >
+                <MaterialIcons
+                  name="print-disabled"
+                  size={20}
+                  color={"#9ca3af"}
+                />
+              </TouchableOpacity>
+            </View>
+            {selectedNetPrinter ? (
+              <View className="border border-gray-300 px-3 py-1.5 rounded mt-2">
+                <View className="flex-row justify-between items-center">
+                  <Text className="font-medium text-base">Net Printer 1</Text>
+                </View>
+                <View className="flex-row justify-between mt-1">
+                  <Text className="text-gray-500 text-medium">
+                    IP: {selectedNetPrinter.host}
+                  </Text>
+                  <Text className="text-gray-500 text-medium">
+                    PORT: {selectedNetPrinter.port}
+                  </Text>
+                </View>
+              </View>
+            ) : (
+              <>
+                <PrimaryInput
+                  value={netPrinter.host}
+                  onChangeText={(host) => {
+                    setNetPrinter((prev) => ({ ...prev, host }));
+                  }}
+                  placeholder="Enter IP address..."
+                  className="text-sm h-[40]"
+                  containerClassName="mt-2"
+                />
+                <PrimaryInput
+                  keyboardType="numeric"
+                  value={netPrinter.port}
+                  onChangeText={(text) => {
+                    const port = text.replace(/[^\d]/g, "");
+                    setNetPrinter((prev) => ({ ...prev, port }));
+                  }}
+                  maxLength={5}
+                  placeholder="Enter port number..."
+                  className="text-sm h-[40]"
+                  containerClassName="mt-2"
+                />
+                <PrimaryButtonSM
+                  onPress={() => {
+                    dispatch(
+                      selectNetPrinter({
+                        host: netPrinter.host,
+                        port: Number(netPrinter.port),
+                      })
+                    );
+                  }}
+                  className="mt-3 h-[45]"
+                >
+                  Connect Net Printer
+                </PrimaryButtonSM>
+              </>
+            )}
+          </View>
+
+          <View className="flex-row justify-between items-center border-y border-gray-300 py-3 px-5 my-5">
             <View className="w-9/12">
               <Text className="font-medium text-base mb-0.5">
                 Print Receipt Automatically
